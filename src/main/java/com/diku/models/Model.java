322ad87f67184d6c2b288b88bf25a813cb6a8b4c
Lukires <lukas.thomas.schilling@gmail.com>
2020-09-18 14:08:12 +0200

    added simple javadocs

--- src/main/java/com/diku/models/Model.java
+++ src/main/java/com/diku/models/Model.java
@@ -16,6 +16,10 @@ public abstract class Model<T> {
     protected final T object;
     protected Document document;
     protected MongoCollection<Document> collection;
+
+    /**
+     * @param object
+     */
     public Model(T object) {
         this.object=object;
         this.collection=getCollection();
@@ -41,16 +45,28 @@ public abstract class Model<T> {
     public abstract MongoCollection<Document> getCollection();
     public abstract String getID();
 
+    /**
+     *
+     */
     public void delete() {
         collection.deleteOne(Filters.eq("_id", getID()));
         init();
     }
 
+    /**
+     * @param key
+     * @param value
+     */
     protected void update(String key, Object value) {
         document.append(key, value);
         collection.updateOne(Filters.eq("_id", getID()), new Document("$set", new Document(key, value)));
     }
 
+    /**
+     * Equals checks whether or not the object stems from the same class path. It also checks if the two objects have the same ID and Collection (Database Table)
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -59,6 +75,9 @@ public abstract class Model<T> {
         return model.getID().equals(((Model<?>) o).getID()) && model.getCollection().equals(((Model<?>) o).getCollection());
     }
 
+    /**
+     * @return
+     */
     @Override
     public int hashCode() {
         int hash = 7;
