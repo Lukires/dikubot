7178fd6c0d89339e33f8764b4496a02c04d734e5
Lukires <lukas.thomas.schilling@gmail.com>
2020-09-28 11:23:46 +0200

    debugging on production ;)

--- src/main/java/com/diku/greeting/JoinListener.java
+++ src/main/java/com/diku/greeting/JoinListener.java
@@ -2,6 +2,7 @@ package com.diku.greeting;
 
 import com.diku.conversation.GuildConversation;
 import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
@@ -9,23 +10,16 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class JoinListener extends ListenerAdapter {
 
-
-
-    //When the bots join a server
-    public void onGuildJoin(GuildJoinEvent e) {
-
-    }
-
     //When a user joins a server
     public void onGuildMemberJoin(GuildMemberJoinEvent e) {
-        User user = e.getUser();
-        user.openPrivateChannel().queue((channel) ->
+        Member member = e.getMember();
+        member.getUser().openPrivateChannel().queue((channel) ->
         {
             MessageBuilder mb = new MessageBuilder();
-            mb.append(user.getAsMention()+" hej og velkommen til DIKU 2020's Discord Server!\n");
+            mb.append(member.getAsMention()).append(" hej og velkommen til DIKU's uofficielle Discord Server!\n");
             mb.append("For at få adgang til hele vores Discord server, kan du bruge kommandoen **!role**");
             channel.sendMessage(mb.build()).queue();
-            GuildConversation.addConversation(new GuildConversation(user, channel, e.getGuild()));
+            GuildConversation.addConversation(new GuildConversation(member.getUser(), channel, e.getGuild()));
         });
     }
 
