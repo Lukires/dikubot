597fe0089e1032a4c3d3028ae5dfa660ed3eb3a1
Lukires <lukas.thomas.schilling@gmail.com>
2021-02-08 14:59:27 +0100

    More groups and uml

--- src/main/java/ninja/diku/ku/Roles.java
+++ src/main/java/ninja/diku/ku/Roles.java
@@ -7,7 +7,13 @@ public enum Roles {
 
     DATALOG("Datalog", false), DIKU("DIKU", false), KU("KU", false),
     MACHINETEACHER("Machine Learning",false), CBS_PROGRAMMING("Datalogi-Økonomi", false),
-    WEEB("Weeb", true), GAMER("Gamer", true), DND("DND", true), RUS("Rus", true), SUNDHED_IT("sundIT/Mat", false);
+    WEEB("Weeb", true), GAMER("Gamer", true), DND("DND", true), RUS("Rus", true), SUNDHED_IT("sundIT/Mat", false),
+    HOLD_1("Hold-1", true), HOLD_2("Hold-2", true), HOLD_3("Hold-3", true),
+    HOLD_4("Hold-4", true), HOLD_5("Hold-5", true), HOLD_6("Hold-6", true),
+    HOLD_7("Hold-7", true), HOLD_8("Hold-8", true), HOLD_9("Hold-9", true),
+    HOLD_10("Hold-10", true), HOLD_11("Hold-11", true), HOLD_12("Hold-12", true),
+    HOLD_13("Hold-13", true), HOLD_14("Hold-14", true), HOLD_15("Hold-15", true),
+    HOLD_16("Hold-16", true),;
 
     public static final Map<String, Roles> nameIndex = new HashMap<>(Roles.values().length);
 
